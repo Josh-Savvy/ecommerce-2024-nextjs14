cd728964b1911688096fced0d271cc1a9925e58d
Josh-Savvy <joshsavyy007@gmail.com>
2024-06-16 13:50:42 +0100

    ui fixes

--- src/components/organisms/home/shop-by-categories/index.tsx
+++ src/components/organisms/home/shop-by-categories/index.tsx
@@ -22,24 +22,28 @@ const ShopbyCategories = () => {
 		<div>
 			<SectionTitle title="Shop by category" />
 			<div className="grid grid-cols-12 gap-3 pt-5">
-				<div className="h-full w-full col-span-4 relative overflow-hidden bg-gray-300 rounded group">
-					<div className="text-white absolute top-0 left-0 h-full w-full group-hover:opacity-100 opacity-0 duration-300 bg-gradient-to-t from-black/80 to-black/10 z-10 p-5 flex flex-col gap-1 items-end justify-end">
-						<h1 className="font-medium tracking-tight leading-none text-lg">{data.data[0].title}</h1>
-						<Link href={`#${data.data[0].id}`} className="hover:underline">
-							<span>Explore</span>
-						</Link>
-					</div>
-					<Image
-						src={data.data[0].imageUrl ? data.data[0].imageUrl : ""}
-						fill
-						alt=""
-						placeholder={`data:image/svg+xml;base64,${toBase64(shimmer(100, 100))}`}
-						priority
-						unoptimized
-						className="bg-gray-300"
-					/>
+				<div className="h-full w-full col-span-5 relative overflow-hidden bg-gray-300 rounded group">
+					{data.data.slice(0, 1).map((cat, index) => (
+						<>
+							<div className="text-white absolute top-0 left-0 h-full w-full group-hover:opacity-100 opacity-0 duration-300 bg-gradient-to-t from-black/80 to-black/10 z-10 p-5 flex flex-col gap-1 items-end justify-end">
+								<h1 className="font-medium tracking-tight leading-none text-lg">{cat.title}</h1>
+								<Link href={`#${cat.id}`} className="hover:underline">
+									<span>Explore</span>
+								</Link>
+							</div>
+							<Image
+								src={cat.imageUrl ? cat.imageUrl : ""}
+								fill
+								alt=""
+								placeholder={`data:image/svg+xml;base64,${toBase64(shimmer(100, 100))}`}
+								priority
+								unoptimized
+								className="bg-gray-300"
+							/>
+						</>
+					))}
 				</div>
-				<div className="h-full w-full col-span-8 relative overflow-hidden grid grid-cols-2 gap-3">
+				<div className="h-full w-full col-span-7 relative overflow-hidden grid grid-cols-2 gap-3">
 					{data.data.slice(1, 4).map((cat, index) => (
 						<CategoryItem key={index} {...cat} />
 					))}
